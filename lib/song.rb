380b2ae5c02ab61dd997c49c5651eacc2068bd64
Carla Holcomb <carla.programmer@gmail.com>
2019-11-17 23:46:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,19 +23,8 @@ class Song
     @@songs
   end
 
-  #def self.reset_all
-    #self.all.clear
-  #end
-
-  #def self.count
-    #self.all.count
-  #end
-
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
