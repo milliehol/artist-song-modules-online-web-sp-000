1be8cec82deef81864017c780b1c06a4ac8fed0f
Carla Holcomb <carla.programmer@gmail.com>
2019-11-17 23:46:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,10 @@
 require 'pry'
 
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
